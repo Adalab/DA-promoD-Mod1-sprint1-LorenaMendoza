5ba9e64c2889b97864bb9f68e35a777ce02e488c
Lorena <lorena.mndzc@gmail.com>
2023-03-08 14:23:32 +0100

    evaluaciones completadas

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # -DA-promoD-Mod1-sprint1-LorenaMendoza
+
+**REPOSITORIO EVALUACIÓN LORENA MENDOZA**
+
+En el presente repositorio se podrán ver los ejercicios de Python y MySQL pertenecientes a la evaluación 1 del primer sprint del módulo1 de la estudiante Lorena Mendoza. 
+
+Los archivos contenidos son:
+
+- Evaluación Python
+- Evaluación SQL 
---
+++ consultas_evaluacion-sql.sql
@@ -0,0 +1,60 @@
+USE sports_analytics;
+
+#Realiza las siguientes consultas:
+
+# 1) Selecciona las victorias máximas y mínimas para cada equipo diferente en la tabla de seasons.
+
+##---De la tabla seasons no se pueden seleccionar esos datos, ya que solo tenemos como información
+##   disponible el identificador de la temporada y la temporada. Dicha consulta habría que realizarla
+##   en stats (pts,win_percent...)
+
+SELECT MAX(w), MIN(w)
+	FROM stats;
+    
+    #Las victorias máximas son 55
+    #Las victorias mínimas son 20
+
+# 2) Obtén los primeros 10 resultados de la tabla de teams.
+
+SELECT *
+	FROM teams
+    ORDER BY team_id
+	LIMIT 10; 
+
+# 3) Selecciona todos los datos de los equipos que tengan entre 20 y 30 victorias de la tabla stats.
+
+SELECT *
+	FROM stats
+	WHERE w BETWEEN 20 AND 30;
+
+# 4) Realiza un conteo de número total de equipos que existen en la tabla teams.
+
+SELECT COUNT (team)
+	FROM teams
+    
+#MYSQL no me permite realizar la búsqueda. Me aparece que la función sports_analytics.COUNT no existe
+#HE AÑADIDO UNA MODIFICIACIÓN A LA TABLA TEAMS (columna TEAM > VALOR NOT NULL), YA QUE NORMALMENTE LA FUNCIÓN COUNT()
+#NO MUESTRA LOS VALORES NULOS, Y PENSABA QUE QUIZÁ PODRÍA ESTAR RELACIONADO,
+#PERO ME SIGUE APARECIENDO EL MISMO ERROR.
+
+	# ALTER TABLE season
+	#MODIFY team_id BIGINT NOT NULL;#
+
+# RESPUESTA (versión tradicional): el número total de equipos es 30.
+
+#5)Calcula la media de todos los puntos conseguidos de la tabla stats
+
+SELECT AVG(pts)
+	FROM stats;
+    #La media de los puntos conseguidos es 111.51666641235352
+
+# 6) Selecciona el máximo de victorias, máximo de puntos y la diferencia entre 
+# el máximo de puntos con la media de puntos como diferencia_media de la tabla stats.
+
+SELECT MAX(w), MAX(pts), (MAX(pts) - AVG(pts)) AS diferencia_media
+	FROM stats;
+    
+##La información solicitada es la siguiente:
+    #número de victorias máximas: 55',
+    #número de puntos máximos: '118.6',
+    #la diferencia entre el MAX(pts) y el AVG(pts): '7.083332061767578'
